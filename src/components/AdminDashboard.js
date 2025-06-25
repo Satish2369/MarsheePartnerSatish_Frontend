cb06157752e79d564763abe269b2ed80bc0714bd
satish2369 <satishmourya410@gmail.com>
2025-06-25 15:32:43 +0530

    user-search modified

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -6,20 +6,20 @@ import axios from "axios";
 import { BASE_URL } from "@/utils/constant";
 
 const AdminDashboard = () => {
-  const [allAccounts, setAllAccounts] = useState([]);
-  const [filteredAccounts, setFilteredAccounts] = useState([]);
+  
+  const [account, setAccount] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [error,setError] = useState("");
 
   const fetchAccounts = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/admin/accounts", {
+      const res = await axios.post(BASE_URL + "/admin/account", {query:searchTerm},{
         withCredentials: true,
       });
 
       if (res.data.success) {
-        setAllAccounts(res.data.data);
-        setFilteredAccounts(res.data.data);
+        setAccount(res.data.data);
+       console.log(account);
       }
     } catch (err) {
       console.error("Failed to fetch accounts:", err);
@@ -28,17 +28,13 @@ const AdminDashboard = () => {
   };
 
   useEffect(() => {
-    fetchAccounts();
-  }, []);
-
-  useEffect(() => {
-    const filtered = allAccounts.filter(
-      (acc) =>
-        acc.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        acc.email.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredAccounts(filtered);
+    if(searchTerm.length>0){
+            fetchAccounts();
+    }
+    
   }, [searchTerm]);
+
+
 
   return (
     <div className="min-h-screen p-6 bg-gray-100">
@@ -52,23 +48,24 @@ const AdminDashboard = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       {error && <p className="text-red-400 texl-[30px] text-center">{error}</p>}
+         
+{account && searchTerm.length>0 &&(
+  <div className="grid gap-4 mt-4">
+    {account.map((acc, index) => {
+      return (
+        <div
+          key={index}
+          className="bg-white rounded-xl shadow-md p-4 border border-gray-200 hover:shadow-lg transition-shadow duration-300"
+        >
+          <h2 className="text-lg font-semibold text-gray-800"> partner name: {acc.name}</h2>
+          <p className="text-sm text-gray-600">emailId : {acc.email}</p>
+        </div>
+      );
+    })}
+  </div>
+)}
 
-      {filteredAccounts.length === 0 ? (
-        <p>No accounts found.</p>
-      ) : (
-        <ul className="space-y-3">
-          {filteredAccounts.map((acc) => (
-            <li
-              key={acc._id}
-              className="bg-white p-4 rounded shadow flex flex-col gap-1"
-            >
-              <span className="font-semibold text-lg">{acc.name}</span>
-              <span className="text-gray-600">{acc.email}</span>
-              <span className="text-sm text-gray-400">Role: {acc.role}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+      
     </div>
   );
 };
